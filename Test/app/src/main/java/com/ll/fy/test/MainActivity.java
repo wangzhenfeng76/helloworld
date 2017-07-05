95bcbf3f626eb2e0b8f1eda324465510e53396e1
wangweigood <1148344468@qq.com>
2017-07-05 11:14:04 +0800

    add a toast
    Signed-off-by: wangweigood <1148344468@qq.com>

--- Test/app/src/main/java/com/ll/fy/test/MainActivity.java
+++ Test/app/src/main/java/com/ll/fy/test/MainActivity.java
@@ -2,12 +2,20 @@ package com.ll.fy.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        Toast.makeText(this,"hello ww",Toast.LENGTH_LONG).show();
     }
 }
