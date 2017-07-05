e4c36f31a5aafe36a7fd51a0904fab02c35f8777
wangweigood <1148344468@qq.com>
2017-07-05 14:11:55 +0800

    completed
    Signed-off-by: wangweigood <1148344468@qq.com>

--- Test/app/src/main/java/com/ll/fy/test/MainActivity.java
+++ Test/app/src/main/java/com/ll/fy/test/MainActivity.java
@@ -3,19 +3,31 @@ package com.ll.fy.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    String b;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setText("A");
     }
 
     public void click(View view) {
-        Toast.makeText(this,"hello aaaaa  ww",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "hello aaaaa  ww", Toast.LENGTH_LONG).show();
+        b = (String) button.getText();
+        if (b.equals("A")) {
+            button.setText("B");
+        } else {
+            button.setText("A");
+        }
+
     }
 }
